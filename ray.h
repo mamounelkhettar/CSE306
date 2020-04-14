523edaa6a82c97af0c28f9288bb88c6faa5c1883
mamounelkhettar <mamoun.elkhettar@polytechnique.edu>
2020-04-14 15:09:06 +0100

    code

--- ray.h
+++ ray.h
@@ -7,9 +7,7 @@ class Ray {
     public :
         Vector o ; // origin vector
         Vector u ; // unit direction
-        //Ray() ;
-        //Ray(const Vector &o, const Vector &u) ;
-        Ray(const Ray &ray) ;
+        
         Ray &operator=(const Ray &ray) {
             o = ray.o ; 
             u = ray.u ;
